af5a4fb3cca47e68c5f21c5e10afa0dba09f8c23
deetz99 <dietrich@wolpert.ca>
2025-03-12 16:46:10 -0700

    stuff

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full mx-auto p-10 flex flex-col gap-10">
+    <h1 class="text-3xl font-bold">From Layer</h1>
+
+    <UButton label="Hello World" />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint'
+  ],
+
+  css: [
+    join(currentDir, './app/assets/css/main.css'),
+  ],
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2025-03-12'
+})
